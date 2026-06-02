5935c39f7c9f9263b1dec1faf9076c3580ff4a1e
Khadeja Qureshi <133877561+khadeja-qureshi@users.noreply.github.com>
2026-06-02 18:30:34 +0500

    Update axiosInstance.js

--- Frontend/src/api/axiosInstance.js
+++ Frontend/src/api/axiosInstance.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:5000",
 });
 
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
